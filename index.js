2991c4a8d58a5472cc1c6d868b2a3a1e749b506f
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-10-30 19:38:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue){
+  return drivers.filter(function(driver){
+    return driver.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue){
+  let newDriverArr = driversWithRevenueOver(drivers, revenue);
+  newDriverArr = newDriverArr.map(function(driver){
+    return driver.name;
+  });
+  return newDriverArr;
+}
+
+
+function exactMatch(drivers, info){
+  return drivers.filter(function(driver){
+    let key = Object.keys(info);
+    return driver[key] === info[key];
+  });
+}
+
+function exactMatchToList(drivers, info){
+  let driverObjs = exactMatch(drivers, info);
+  return driverObjs.map(function(driver){return driver.name});
+}
